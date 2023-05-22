a5249e5cb571c3da0a6711c0449e593021785165
Evangelion Manuhutu <86818967+kaks-ev@users.noreply.github.com>
2023-05-22 16:41:53 +0900

    update script

--- Scripts/projects/ORigin-Editor.lua
+++ Scripts/projects/ORigin-Editor.lua
@@ -56,13 +56,13 @@ filter "system:windows"
     systemversion "latest"
     postbuildcommands {
         -- Copy C# Project Assets
-        "{COPYDIR} %{prj.location}SandboxProject/Binaries {wks.location}Binaries/%{cfg.buildcfg}/ORigin/SandboxProject/Binaries",
-        "{COPYDIR} %{prj.location}SandboxProject/Assets {wks.location}Binaries/%{cfg.buildcfg}/ORigin/SandboxProject/Assets",
+        "{COPYDIR} %{prj.location}SandboxProject/Binaries %{wks.location}Binaries/%{cfg.buildcfg}/ORigin/SandboxProject/Binaries",
+        "{COPYDIR} %{prj.location}SandboxProject/Assets %{wks.location}Binaries/%{cfg.buildcfg}/ORigin/SandboxProject/Assets",
 
-        "{COPYDIR} %{prj.location}mono {wks.location}Binaries/%{cfg.buildcfg}/ORigin/mono",
-        "{COPYDIR} %{prj.location}Resources {wks.location}Binaries/%{cfg.buildcfg}/ORigin/Resources",
+        "{COPYDIR} %{prj.location}mono %{wks.location}Binaries/%{cfg.buildcfg}/ORigin/mono",
+        "{COPYDIR} %{prj.location}Resources %{wks.location}Binaries/%{cfg.buildcfg}/ORigin/Resources",
 
-        "{COPY} %{prj.location}imgui.ini {wks.location}Binaries/%{cfg.buildcfg}/ORigin",
+        "{COPY} %{prj.location}imgui.ini %{wks.location}Binaries/%{cfg.buildcfg}/ORigin",
 
         -- Copy 3rd Party Library
         "{COPY} %{wks.location}ORigin/vendor/Assimp/lib/x64/assimp-vc143-mt.dll %{wks.location}Binaries/%{cfg.buildcfg}/ORigin"
